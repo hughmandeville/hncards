550c310956851d13bbfae825df42d9fdfc935766
Hubert Mandeville <hubert.mandeville@nytimes.com>
2022-01-23 00:26:23 -0500

    added constructor

--- client/src/TopStoriesPage.js
+++ client/src/TopStoriesPage.js
@@ -3,6 +3,15 @@ import "./TopStoriesPage.css";
 
 // TopStoriesPage - Render Top Stories page.
 class TopStoriesPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showMenu: false,
+    };
+  }
+
+  componentDidMount() {}
+
   render() {
     return <div id="ts-page">TBD</div>;
   }
