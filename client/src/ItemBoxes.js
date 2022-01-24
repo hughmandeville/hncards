f7952456245f0da46af1601eb2d003e0aec34c68
Hubert Mandeville <hubert.mandeville@nytimes.com>
2022-01-23 23:43:17 -0500

    set time

--- client/src/ItemBoxes.js
+++ client/src/ItemBoxes.js
@@ -33,11 +33,13 @@ function ItemBoxes(props) {
         />
       );
     }
+
     pub = (
       <div className="publisher">
         {pubIcon}
         <div className="publisher-text">
-          {item.publisher} <div className="dot">&bull;</div> time
+          {item.publisher} <div className="dot">&bull;</div>{" "}
+          {timeSince(item.time)}
         </div>
       </div>
     );
@@ -69,8 +71,11 @@ function ItemBoxes(props) {
     }
     footer = (
       <div className="box-footer">
-        by {item.by} <div className="dot">&bull;</div> {item.score} pts{" "}
-        {comments}
+        by{" "}
+        <a href={"https://news.ycombinator.com/user?id=" + item.by}>
+          {item.by}
+        </a>{" "}
+        <div className="dot">&bull;</div> {item.score} pts {comments}
       </div>
     );
 
@@ -88,6 +93,48 @@ function ItemBoxes(props) {
   });
 
   return <div className="boxes">{boxes}</div>;
+}
+
+// Return time since in human readable format (e.g. "2 hrs", "on Sat").
+function timeSince(ts) {
+  var seconds = Math.floor((new Date() - ts * 1000) / 1000);
+
+  var interval = seconds / 31536000;
+
+  if (interval > 1) {
+    if (Math.floor(interval) === 1) {
+      return Math.floor(interval) + " yr";
+    }
+    return Math.floor(interval) + " yrs";
+  }
+  interval = seconds / 2592000;
+  if (interval > 1) {
+    if (Math.floor(interval) === 1) {
+      return Math.floor(interval) + " mon";
+    }
+    return Math.floor(interval) + " mons";
+  }
+  interval = seconds / 86400;
+  if (interval > 1) {
+    // If 2 to 5 days ago, show day of week (e.g. "on Sat").
+    if (Math.floor(interval) >= 2 && Math.floor(interval) <= 5) {
+      const d = new Date(ts * 1000);
+      return "on " + d.toLocaleDateString("en-US", { weekday: "short" });
+    }
+    return Math.floor(interval) + " d";
+  }
+  interval = seconds / 3600;
+  if (interval > 1) {
+    if (Math.floor(interval) === 1) {
+      return "1 hr";
+    }
+    return Math.floor(interval) + " hrs";
+  }
+  interval = seconds / 60;
+  if (interval > 1) {
+    return Math.floor(interval) + " mins";
+  }
+  return Math.floor(seconds) + " secs";
 }
 
 export default ItemBoxes;
