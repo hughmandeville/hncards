a1f71690ae6ddbeb0bffdc60e435a4ea172a36f2
Hubert Mandeville <hubert.mandeville@nytimes.com>
2022-01-25 00:35:49 -0500

    removed routes

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,7 @@
 import React from "react";
 import { render } from "react-dom";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+//import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
 import { TopStoriesPage } from "./TopStoriesPage";
 
-render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<TopStoriesPage />}></Route>
-    </Routes>
-  </BrowserRouter>,
-  document.getElementById("root")
-);
+render(<TopStoriesPage />, document.getElementById("root"));
