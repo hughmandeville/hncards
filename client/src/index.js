915d5819f63798b65f7c447cbfe6c1d99551ad94
lpierfelice <lorenzo.pierfelice@nytimes.com>
2022-01-27 19:55:08 +0100

    fix: refactoring react components

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,6 @@ import React from "react";
 import { render } from "react-dom";
 //import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
-import { TopStoriesPage } from "./TopStoriesPage";
+import TopStoriesPage from "./TopStoriesPage";
 
 render(<TopStoriesPage />, document.getElementById("root"));
