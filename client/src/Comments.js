52f6d95c15d7a39b3cfa8b52811967e64ca6bfe6
lpierfelice <lorenzo.pierfelice@nytimes.com>
2022-01-27 22:58:11 +0100

    fix: comments!

--- client/src/Comments.js
+++ client/src/Comments.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import './Comments.css';
 
 // Comments component
 const Comments = ({ kids, id }) => (
   <span>
     <div className="dot">&bull;</div>{' '}
-    <a href={'https://news.ycombinator.com/item?id=' + id}>{kids.length} comments</a>
+    <a href={'https://news.ycombinator.com/item?id=' + id}>
+      <button type="button" className="commentsButtonClass" aria-label={kids.length}>
+        <i className="iconClass">
+          <span className="textClass">{kids.length}</span>
+        </i>
+      </button>
+    </a>
   </span>
 );
 
