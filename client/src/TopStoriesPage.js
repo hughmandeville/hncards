1808b7560a25afc3870e615216b8f81403df86a5
Hubert Mandeville <hubert.mandeville@nytimes.com>
2022-01-29 13:39:41 -0500

    format nice error page

--- client/src/TopStoriesPage.js
+++ client/src/TopStoriesPage.js
@@ -1,22 +1,27 @@
-import React, { useEffect, useState }  from "react";
-import ItemBoxes from "./ItemBoxes";
-import NavBar from "./NavBar";
-import "./TopStoriesPage.css";
+import React, { useEffect, useState } from 'react';
+import ItemBoxes from './ItemBoxes';
+import NavBar from './NavBar';
+import './TopStoriesPage.css';
 
 // TopStoriesPage - Render Top Stories page.
 const TopStoriesPage = () => {
   const [error, setError] = useState();
   const [items, setItems] = useState();
 
+  let hnURL =
+    'https://raw.githubusercontent.com/hughmandeville/hnui/main/client/public/hn_topstories.json';
+  if (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1') {
+    hnURL = '/hnui/hn_topstories.json';
+  }
+
   useEffect(() => {
-    fetch(
-      "https://raw.githubusercontent.com/hughmandeville/hnui/main/client/public/hn_topstories.json"
-    )
-      .then((response) => response.json())
-      .then((items) => {
+    fetch(hnURL)
+      .then(response => response.json())
+      .then(items => {
         setItems(items);
       })
-      .catch((error) => {
+      .catch(error => {
+        console.log('Error:', error);
         setError(error);
       });
   });
@@ -24,10 +29,30 @@ const TopStoriesPage = () => {
   return (
     <div id="ts-page">
       <NavBar />
-      <div>{error ? <div className="error">{error}</div> : <div className="boxes">
-        <ItemBoxes items={items} />
-      </div>}</div>
-    </div>)
-}
+      <div>
+        {error ? (
+          <div className="error">
+            <img src="/hnui/meh.png" />
+            <p>
+              There was a problem getting the <a href={hnURL}>top stories data</a>.
+            </p>
+            <p>
+              Please try again later or view the{' '}
+              <a href="https://news.ycombinator.com/">Hacker News site</a>.
+            </p>
+            <p>&bull;</p>
+            <p>
+              <i>{error.toString()}</i>
+            </p>
+          </div>
+        ) : (
+          <div className="boxes">
+            <ItemBoxes items={items} />
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
 
 export default TopStoriesPage;
