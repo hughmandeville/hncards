9d488c3a967dfdd8af095ae30fca209df6efd364
Hubert Mandeville <hubert.mandeville@nytimes.com>
2022-01-31 13:11:28 -0500

    added loading text

--- client/src/ItemBoxes.js
+++ client/src/ItemBoxes.js
@@ -4,7 +4,11 @@ import Card from './Card';
 
 // ItemBoxews - Render item boxes.
 const ItemBoxes = ({ items }) => {
-  if (!items?.length) return null;
+  if (!items?.length) {
+    return (<div className="loading">
+    fetching top stories...
+    </div>)
+  }
 
   return (
     <div className="boxes">
